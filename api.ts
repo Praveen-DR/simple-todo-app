4455441d0009e0577adbdcbdb941cdc1bcd3b357
Praveen D R <praveendr07@gmail.com>
2025-06-08 21:56:54 +0530

    Update: add task feature implemented

--- api.ts
+++ api.ts
@@ -1,10 +1,22 @@
+import { todo } from "node:test";
 import { ITask } from "./types/tasks";
 
-const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+const baseUrl = 'http://localhost:3001';
 
 export const getAllTodos = async (): Promise<ITask[]> => {
-    const res = await fetch(`${baseUrl}/tasks`);
+    const res = await fetch(`${baseUrl}/tasks`, {cache: 'no-store'});
     const todos = await res.json();
     return todos;
 }
 
+export const addTodo = async (todo: ITask): Promise<ITask> => {
+    const res = await fetch(`${baseUrl}/tasks`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(todo)
+    })
+    const newTodo = await res.json();
+    return newTodo;
+}
--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,22 +1,22 @@
 interface ModalProps {
     modalOpen: boolean;
     setModalOpen:(open : boolean) => boolean | void;
+    children: React.ReactNode
 }
 
-const Modal: React.FC<ModalProps> = ({modalOpen, setModalOpen}) => {
+const Modal: React.FC<ModalProps> = ({modalOpen, setModalOpen, children}) => {
     return (
         <>
             <div className={`modal ${modalOpen ? "modal-open" : ""}`}>
                 <div className="modal-box">
-                    <h3 className="text-lg font-bold">Hello!</h3>
-                    <p  className="py-4">This modal works with a hidden checkbox!</p>
+                    {children}
                 </div>
                 <label 
                 onClick={() => setModalOpen(false)} 
                 className="modal-backdrop" >Close</label>
             </div>
         </>
-    )
+    )   
 }
 
 export {Modal}
